6d303e65eb107690079de76f7c9a6c9149528c46
Galina Teplitskaya <mflucifer66x@gmail.com>
2018-02-24 23:59:12 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let SymbolArray = equation.split(' ');
+    let a = Number(SymbolArray[0]);
+    let b = 0;
+    let c = 0;
+    if (SymbolArray[3] === '-') {
+        b = Number(-SymbolArray[4]);
+    }
+    else {
+        b = Number(SymbolArray[4]);
+    }
+
+    if (SymbolArray[7] === '-') {
+        c = Number(-SymbolArray[8]);
+    }
+    else {
+        c = Number(SymbolArray[8]);
+    }
+    let D = ((b * b) - (4 * a * c));
+    let x1 = 0;
+    let x2 = 0;
+    let FinishArray = [];
+    if (D > 0) {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
+        FinishArray.push(Math.round(x1));
+        FinishArray.push(Math.round(x2));
+
+    }
+    return FinishArray;
+
+};
